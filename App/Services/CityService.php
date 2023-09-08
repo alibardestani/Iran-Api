003cf4fd0b0276cecfcd2cab05f746c008ec53de
aliwdxa <alibardestani201796@gmail.com>
2023-09-08 18:24:44 +0330

    add crud options in services

--- App/Services/CityService.php
+++ App/Services/CityService.php
@@ -2,6 +2,20 @@
 namespace App\Services;
 class CityService{
     public function getCities($data){
-        return $data;
+        return getCities($data);
     }
+
+    public function creatCity($data){
+        return addCity($data);
+    }
+
+    public function updateCityName($city_id, $name){
+        return changeCityName($city_id, $name);
+    }
+
+    public function deleteCity($city_id){
+        return deleteCity($city_id);
+    }
+
+
 }
--- api/v1/cities/index.php
+++ api/v1/cities/index.php
@@ -4,8 +4,8 @@ use \App\Services\CityService;
 use \App\Utilities\Response;
 
 $request_method = $_SERVER['REQUEST_METHOD'];
-
 $request_body = json_decode(file_get_contents('php://input'),true);
+$city_service = new CityService();
 
 
 switch ($request_method){
@@ -15,13 +15,26 @@ switch ($request_method){
             'province_id' => $province_id
         ];
         $response = getCities($request_data);
+        if (empty($response))
+            Response::respondAndDie($response,Response::HTTP_NOT_FOUND);
         Response::respondAndDie($response,Response::HTTP_OK);
     case 'POST':
-        Response::respondAndDie(['POST Request'],Response::HTTP_OK);
+        if(!isValidCity($request_body))
+            Response::respondAndDie(['Invalid City Data ..'],Response::HTTP_NOT_ACCEPTABLE);
+        $response = $city_service->creatCitY($request_body);
+        Response::respondAndDie($response,Response::HTTP_CREATED);
     case 'PUT':
-        Response::respondAndDie(['PUT Request'],Response::HTTP_OK);
+        [$city_id,$city_name] = [$request_body['city_id'],$request_body['name']];
+        if(!is_numeric($city_id) or empty($city_name))
+            Response::respondAndDie(['Invalid City Data ..'],Response::HTTP_NOT_ACCEPTABLE);
+        $result = $city_service->updateCityName($city_id,$city_name);
+        Response::respondAndDie($result,Response::HTTP_OK);
     case 'DELETE':
-        Response::respondAndDie(['DELETE Request'],Response::HTTP_OK);
+        $city_id = $_GET['city_id'] ?? null;
+        if(!is_numeric($city_id) or is_null($city_id))
+            Response::respondAndDie(['Invalid City id ..'],Response::HTTP_NOT_ACCEPTABLE);
+        $result = $city_service->deleteCity($city_id);
+        Response::respondAndDie($result,Response::HTTP_OK);
     default:
         Response::respondAndDie(['Invalid request Method'],Response::HTTP_METHOD_NOT_ALLOWED);
 }
